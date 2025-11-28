e2f1917d19a81490f9ae43e8eb904e05d799ef7f
Bhavika Radadiya <bhavikaradadiya01@gmail.com>
2025-11-29 01:48:27 +0530

    Add files via upload

---
+++ sql/order_product.sql
@@ -0,0 +1,25 @@
+/*
+-- Query: SELECT * FROM online_market_data_warehouse.order_product
+LIMIT 0, 500
+
+-- Date: 2025-11-29 01:40
+*/
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1111,201,1,102);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1113,208,1,105);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1114,203,1,103);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1115,209,1,105);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1116,208,1,103);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1117,204,1,102);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1118,201,1,101);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1119,208,1,104);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1120,204,1,102);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1121,201,1,102);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1122,201,1,102);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1123,206,1,104);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1124,206,1,103);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1125,205,1,103);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1126,205,1,101);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1127,201,1,102);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1128,210,1,105);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1129,210,1,101);
+INSERT INTO `` (`O_ID`,`P_ID`,`Quentity`,`V_ID`) VALUES (1112,209,2,105);
---
+++ sql/vendor.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM online_market_data_warehouse.vendor
+LIMIT 0, 500
+
+-- Date: 2025-11-29 01:43
+*/
+INSERT INTO `` (`V_ID`,`Name`,`Location`,`Rating`) VALUES (101,'Alex','Berlin',6);
+INSERT INTO `` (`V_ID`,`Name`,`Location`,`Rating`) VALUES (102,'Vencent','Hamberg',5);
+INSERT INTO `` (`V_ID`,`Name`,`Location`,`Rating`) VALUES (103,'Karlos','Munich',4);
+INSERT INTO `` (`V_ID`,`Name`,`Location`,`Rating`) VALUES (104,'Marianna','Berlin',10);
+INSERT INTO `` (`V_ID`,`Name`,`Location`,`Rating`) VALUES (105,'Corneli','Berlin',8);
